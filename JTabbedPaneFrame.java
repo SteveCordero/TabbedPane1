7b8ba88791237ab5b17d595ea53d33c3aae74aaf
Steve Cordero <stevecords2008@gmail.com>
2021-03-01 19:19:07 +0000

    Initial commit

---
+++ JTabbedPaneFrame.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class JTabbedPaneFrame extends JFrame
+{
+  public JTabbedPaneFrame()
+  {
+    super("Steve's JTabbedPane Demo");
+
+  }//end defaults and only constructor
+}//end class
---
+++ Main.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class Main
+{
+  public static void main(String args[])
+  {
+    JTabbedPaneFrame tpf = new JTabbedPaneFrame();
+    tpf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    tpf.setSize(450,400);//this size is up to you
+    tpf.setVisible(true);
+
+  }//end main method
+}//end Main Class
